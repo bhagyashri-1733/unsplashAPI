a63aca01152ebd59b908e2f558f44337e2e2c987
bhagyashri-1733 <bhagyashrithombre.it@gmail.com>
2024-01-02 23:08:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from "axios";
+import { useState } from 'react';
 
 function App() {
+  const[image,setImage]=useState([])
+  const getImage = ()=>{
+   axios.get("https://api.unsplash.com/search/photos?page=1&query=office&client_id=JrKfwP0CGiQpezUS4RPA1uFdZWMksD-dfbsy0UqgSSU")
+    .then((response)=>{
+      //console.log(response)
+      setImage(response.data.results)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <input className="search"
+          type='text'
+          placeholder='search'/>
+        <button className="btn" onClick={getImage}>Get Image</button>
+        </div>
+      </div>
     </div>
+    <div className="container1">
+      <div className='row1'>
+       {
+        image.map((value,index)=>{
+          return(
+            <div key={index}className='col-41'>
+              <div className="card">
+                <img src={value.urls.small} alt='image'/>
+              </div>
+            </div>
+          )
+        })
+       }
+
+      </div>
+    </div>
+    </div>
+    
+    </>
   );
 }
 
